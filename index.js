ef11d8939d3539915eb3eda40b4e46414f3e011f
Tania Manco <belalov.tania16@gmail.com>
2019-11-25 19:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let mondayWork = function(activity = 'go to the office'){
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(visual = "*"){
+let wrapAdjective = function(visual ='*'){
   return function(special = 'special'){
     return `You are ${visual}${special}${visual}!`
   }
