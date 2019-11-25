0efa10ab0839de23c8ea419014b9705032cf3fc8
Tania Manco <belalov.tania16@gmail.com>
2019-11-25 19:06:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ function saturdayFun(activity = 'roller-skate'){
   return `This Saturday, I want to ${activity}!`
 }
 
-function 
+function mondayWork(activity = 'go to the office'){
+  return `This Monday, I will ${activity}`
+}
